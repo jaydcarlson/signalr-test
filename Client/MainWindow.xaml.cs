b383e61b86b1e41657932987e33b351dca63d58b
Jay Carlson <jcarlson@unl.edu>
2016-12-23 00:14:46 -0600

    Initial add

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            var hubConnection = new HubConnection("http://localhost:8080");
+            IHubProxy myHub = hubConnection.CreateHubProxy("MyHub");
+            myHub.On<string>("SayHello", SayHello);
+            await hubConnection.Start();
+        }
+
+        void SayHello(string myString)
+        {
+            textBlock.Dispatcher.Invoke(() =>
+            {
+                textBlock.Text = myString;
+            });
+            
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Libraries.Displays;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Owin;
+using Microsoft.Owin.Hosting;
+using System.Threading;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                //Console.ReadLine();
+                while(true)
+                {
+                    var hubContext = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+                    hubContext.Clients.All.SayHello("The current server time is: " + DateTime.Now.ToLongTimeString());
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    public class MyHub : Hub
+    {
+        public void Send()
+        {
+            Debug.WriteLine("Send() method called on server");
+            Clients.All.SayHello("Hey, world!");
+        }
+    }
+}
